7a487b401a8aa27b5c7c2ffbe587f52ad2310b9b
mmoraisjr <jr.morais.marcos@gmail.com>
2023-01-11 23:05:23 -0300

    create method post

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,22 +1,27 @@
-import { conectaApi } from "./conectaapi";
+import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard() {
+function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
-    video.className = "video__item";
-    video.innerHTML = `<iframe width="100%" height="72%" src="https://www.youtube.com/embed/pA-EgOaF23I"
-    title="YouTube video player" frameborder="0"
+    video.className = "videos__item";
+    video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}" frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
     allowfullscreen></iframe>
 <div class="descricao-video">
-    <img src="./img/logo.png" alt="logo canal alura">
-    <h3>Qual é o melhor hardware para programação com Mario Souto</h3>
-    <p>236 mil visualizações</p>
+    <img src="${imagem}" alt="logo canal alura">
+    <h3>${titulo}</h3>
+    <p>${descricao}</p>
 </div>`
+
     return video;
 }
 
 async function listaVideos() {
-    const lista = await conectaApi.listaVideos();
+    const listaApi = await conectaApi.listaVideos();
+    listaApi.forEach(e => lista.appendChild(
+        constroiCard(e.titulo, e.descricao, e.url, e.imagem)));
 }
+
+listaVideos();
