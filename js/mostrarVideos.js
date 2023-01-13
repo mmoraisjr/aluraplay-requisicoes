1091101b71f8186d905c20c135e2902865ee80e1
mmoraisjr <jr.morais.marcos@gmail.com>
2023-01-12 23:31:59 -0300

    create search function

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function constroiCard(titulo, descricao, url, imagem) {
+export default function constroiCard(titulo, descricao, url, imagem) {
     const video = document.createElement("li");
     video.className = "videos__item";
     video.innerHTML = `<iframe width="100%" height="72%" src="${url}"
@@ -20,8 +20,8 @@ function constroiCard(titulo, descricao, url, imagem) {
 
 async function listaVideos() {
     const listaApi = await conectaApi.listaVideos();
-    listaApi.forEach(e => lista.appendChild(
-        constroiCard(e.titulo, e.descricao, e.url, e.imagem)));
+    listaApi.forEach(element => lista.appendChild(
+        constroiCard(element.titulo, element.descricao, element.url, element.imagem)));
 }
 
 listaVideos();
